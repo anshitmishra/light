bf2dfe67cec58ff6d1c70d4b201d89d2d5bc018a
anshitmishra <anshitmishra03@gmail.com>
2023-03-14 09:22:12 +0530

    new added

--- pages/team.js
+++ pages/team.js
@@ -9,33 +9,116 @@ export default function Team() {
       <div className={styles.container}>
         <div className={styles.mainContact}>
           <div className={styles.mainContactHolder}>
+
+          
             <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
               <div className={styles.mainContactCardImage}>
-                <img src="/pranjal.jpg" alt="anshit" />
+                <img src="/team/7.jpg" alt="Himanshu Dewangan" />
               </div>
               <div className={styles.cardDetails}>
-                <p>daksh jha</p>
-                <span>Team Coordinator</span>
+                <p>Himanshu Dewangan</p>
+                <span>Head</span>
               </div>
             </div>
+
             <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
               <div className={styles.mainContactCardImage}>
-                <img src="/anshit2.jpeg" alt="anshit" />
+                <img src="/team/10.webp" alt="Ayush Gupta" />
               </div>
               <div className={styles.cardDetails}>
-                <p>anshit mishra</p>
-                <span>Team Coordinator</span>
+                <p>Ayush Gupta</p>
+                <span>Head</span>
               </div>
             </div>
+
+
             <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
               <div className={styles.mainContactCardImage}>
-                <img src="/rahul.jpg" alt="rahul" />
+                <img src="/team/8.jpeg" alt="Isha" />
               </div>
               <div className={styles.cardDetails}>
-                <p>deepanshi rai</p>
-                <span>Team Coordinator</span>
+                <p>Isha</p>
+                <span>Head</span>
               </div>
             </div>
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/9.jpg" alt="Mohini" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Mohini</p>
+                <span>Head</span>
+              </div>
+            </div>
+
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/1.webp" alt="Shobhita Bhatt" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Shobhita Bhatt</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/2.jpg" alt="Yashaswini Bakshi" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Yashaswini Bakshi</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/3.jpg" alt="Richa uchariya" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Richa uchariya</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/4.jpg" alt="Nisha soni" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Nisha soni</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/5.jpg" alt="Deeksha Pokhariya" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Deeksha Pokhariya</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+
+            <div className={styles.mainContactCard} data-aos="zoom-in" data-aos-duration="2000">
+              <div className={styles.mainContactCardImage}>
+                <img src="/team/6.png" alt="Amisha Arora" />
+              </div>
+              <div className={styles.cardDetails}>
+                <p>Amisha Arora</p>
+                <span>Team member</span>
+              </div>
+            </div>
+
+
+
           </div>
         </div>
         <div className={styles.footer}>
--- src/page.js
+++ src/page.js
@@ -3,6 +3,7 @@ import Header from '../src/header'
 import React from 'react';
 import Link from 'next/link';
 import { BsArrowRight, BsTelephone, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 import AOS from "aos";
 import "aos/dist/aos.css";
 const Home = () => {
@@ -25,8 +26,8 @@ const Home = () => {
                 <div style={{ position: "relative", width: "100%", height: "80px" }}></div>
                 <div className={styles.container}>
                     <p id={styles.design1} data-aos="fade-up"
-                        data-aos-duration="2000"><img src="./design1.svg" alt="design one" /></p>
-                  
+                        data-aos-duration="2000"><img src="./d1.svg" alt="design one" /></p>
+
                     <div className={styles.SubOnecontainer}>
                         <div className={styles.SubOnecontainerHolder}>
                             <h1>{"<>"} Social spark {"</>"}</h1>
@@ -34,7 +35,7 @@ const Home = () => {
                             <button >Register Now <BsArrowRight style={{ margin: "0px 5px" }} /></button>
                             <p>2023 biggest Hackathon. join us now and win amazing prizepool</p>
                         </div>
-                        <p id={styles.design2} data-aos="fade-left" data-aos-duration="3000"><img src="./design2.svg" alt="design one" /></p>
+                        <p id={styles.design2} data-aos="fade-left" data-aos-duration="3000"><img src="./d2.svg" alt="design one" /></p>
 
                     </div>
                     <div className={styles.SubTwocontainer} data-aos="fade-up"
@@ -75,12 +76,14 @@ const Home = () => {
                     <div className={styles.containerTwoSubTwo} >
                         <h1>Event Rules</h1>
                         <ul>
-                            <li data-aos="fade-right" data-aos-duration="1000">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                            <li data-aos="fade-right" data-aos-duration="1050">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                            <li data-aos="fade-right" data-aos-duration="1100">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                            <li data-aos="fade-right" data-aos-duration="1150">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                            <li data-aos="fade-right" data-aos-duration="1200">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                            <li data-aos="fade-right" data-aos-duration="1250">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                            <li data-aos="fade-right" data-aos-duration="1000">We welcome attendees from all backgrounds, genders and geographies.</li>
+                            <li data-aos="fade-right" data-aos-duration="1050">Participants may register as an individual or as a team with a maximum of three members per team. Only one person per team needs to apply.</li>
+                            <li data-aos="fade-right" data-aos-duration="1100">It will be a  7 day long hackathon.Mode will be online.</li>
+                            <li data-aos="fade-right" data-aos-duration="1150">An introductory webinar will be conducted to  explain the complete process of hackathon</li>
+                            <li data-aos="fade-right" data-aos-duration="1200">Participants can use any development  platform or IDE to write their code.</li>
+                            <li data-aos="fade-right" data-aos-duration="1250">The project will not be considered if submitted after the deadline.</li>
+                            <li data-aos="fade-right" data-aos-duration="1300">The projects will be evaluated based on criteria such as impact, innovation, feasibility, and presentation. The judges' decision will be final and binding..The jury will be composed of a group of members chosen from the most reliable professionals of the organisation as well as businessmen and startup holders.</li>
+                            <li data-aos="fade-right" data-aos-duration="1350">The winning teams will receive prizes, including cash prizes, and goodies.</li>
                         </ul>
                     </div>
                 </div>
@@ -89,12 +92,9 @@ const Home = () => {
 
                     <div className={styles.containerFourSubOne} data-aos="fade-left" data-aos-duration="1000" >
                         <h1>Event Guidlines</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <p>Social Spark is a 7 day long virtual hackathon which is being organised by LiGHT Gwalior Sithouli.The event will take place from Wednesday, March 22nd, 2023  to Wednesday, March 29, 2023.During the event, participants will work in teams or individually to develop prototypes or solutions to address social and environmental challenges. The challenges will be related to themes such as healthcare, education, climate change, poverty, and more.The event will kick off with an opening ceremony where the participants will be briefed on the challenge.
+                            So, join us for Social Spark Hackathon and be a part of a community committed to creating social good through technology.It is a great opportunity to learn new skills, network with like-minded individuals, and make a positive impact.
+                        </p>
                     </div>
                     <div className={styles.containerFourSubTwo} data-aos="fade-right" data-aos-duration="1000">
                         <img src="/details.png" ></img>
@@ -115,7 +115,7 @@ const Home = () => {
                                 <span>Team Coordinator</span>
                             </div>
                             <div className={styles.cardContact}>
-                                <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
+                                <p data-aos="fade-left" data-aos-duration="1000"><AiOutlineMail /></p>
                                 <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
                                 <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
                             </div>
@@ -129,7 +129,7 @@ const Home = () => {
                                 <span>Team Coordinator</span>
                             </div>
                             <div className={styles.cardContact}>
-                                <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
+                                <p data-aos="fade-left" data-aos-duration="1000"><AiOutlineMail /></p>
                                 <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
                                 <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
                             </div>
@@ -143,7 +143,7 @@ const Home = () => {
                                 <span>Team Coordinator</span>
                             </div>
                             <div className={styles.cardContact}>
-                                <p data-aos="fade-left" data-aos-duration="1000"><BsTelephone /></p>
+                                <p data-aos="fade-left" data-aos-duration="1000"><AiOutlineMail /></p>
                                 <p data-aos="fade-left" data-aos-duration="1400"><BsInstagram /></p>
                                 <p data-aos="fade-left" data-aos-duration="1800"><BsLinkedin /></p>
                             </div>
